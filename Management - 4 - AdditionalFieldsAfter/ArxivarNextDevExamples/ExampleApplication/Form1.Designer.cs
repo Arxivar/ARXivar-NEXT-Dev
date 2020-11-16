ad44c332c753baf075473a0dfa8c5752d74bae28
Massimo Conforti <m.conforti@abletech.it>
2020-11-16 12:23:36 +0100

    Add Management additional fields examples

---
+++ Management - 4 - AdditionalFieldsAfter/ArxivarNextDevExamples/ExampleApplication/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace ExampleApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tokenValue = new System.Windows.Forms.Label();
+            this.tokenLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.userTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.aooGrid = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.docTypeGrid = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.aooGrid)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.docTypeGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 663);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1087, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1087, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tokenValue);
+            this.splitContainer1.Panel1.Controls.Add(this.tokenLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.passwordTxt);
+            this.splitContainer1.Panel1.Controls.Add(this.userTxt);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1087, 638);
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // tokenValue
+            // 
+            this.tokenValue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tokenValue.AutoEllipsis = true;
+            this.tokenValue.Location = new System.Drawing.Point(462, 0);
+            this.tokenValue.Name = "tokenValue";
+            this.tokenValue.Size = new System.Drawing.Size(3426, 50);
+            this.tokenValue.TabIndex = 6;
+            // 
+            // tokenLabel
+            // 
+            this.tokenLabel.AutoSize = true;
+            this.tokenLabel.ForeColor = System.Drawing.Color.Red;
+            this.tokenLabel.Location = new System.Drawing.Point(353, 4);
+            this.tokenLabel.Name = "tokenLabel";
+            this.tokenLabel.Size = new System.Drawing.Size(103, 13);
+            this.tokenLabel.TabIndex = 5;
+            this.tokenLabel.Text = "Token non presente";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login Management";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(69, 26);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.Size = new System.Drawing.Size(126, 20);
+            this.passwordTxt.TabIndex = 3;
+            // 
+            // userTxt
+            // 
+            this.userTxt.Location = new System.Drawing.Point(69, 4);
+            this.userTxt.Name = "userTxt";
+            this.userTxt.Size = new System.Drawing.Size(126, 20);
+            this.userTxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Utente";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1087, 584);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.splitContainer2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1079, 558);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Aoo";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.aooGrid);
+            this.splitContainer2.Size = new System.Drawing.Size(1073, 552);
+            this.splitContainer2.SplitterDistance = 195;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Get Aoo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // aooGrid
+            // 
+            this.aooGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.aooGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.aooGrid.Location = new System.Drawing.Point(0, 0);
+            this.aooGrid.Name = "aooGrid";
+            this.aooGrid.Size = new System.Drawing.Size(874, 552);
+            this.aooGrid.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.splitContainer3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1079, 558);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "Document Types and Additional Fields";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.button4);
+            this.splitContainer3.Panel1.Controls.Add(this.button3);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.docTypeGrid);
+            this.splitContainer3.Size = new System.Drawing.Size(1073, 552);
+            this.splitContainer3.SplitterDistance = 195;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Get DocType Additional Fields ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Get Document Types";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // docTypeGrid
+            // 
+            this.docTypeGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.docTypeGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.docTypeGrid.Location = new System.Drawing.Point(0, 0);
+            this.docTypeGrid.Name = "docTypeGrid";
+            this.docTypeGrid.Size = new System.Drawing.Size(874, 552);
+            this.docTypeGrid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 685);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Example WebApi Application";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.aooGrid)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.docTypeGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label tokenLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.TextBox userTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tokenValue;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView aooGrid;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView docTypeGrid;
+    }
+}
+
---
+++ Management - 4 - AdditionalFieldsAfter/ArxivarNextDevExamples/ExampleApplication/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IO.Swagger.Management.Client;
+using IO.Swagger.Management.Model;
+using IO.Swagger.Model;
+
+namespace ExampleApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string _appId = Settings.ExampleSettings.AppName;
+        private string _secret = Settings.ExampleSettings.AppSecret;
+        private string _apiUrl = Settings.ExampleSettings.ApiBaseUrl;
+
+        private string _authToken;
+        private string _refreshToken;
+
+        public IO.Swagger.Management.Client.Configuration Configuration
+        {
+            get
+            {
+                //Build a configuration object with the Token provided during login procedure or refresh token procedure
+                return new Configuration()
+                {
+                    BasePath = _apiUrl,
+                    ApiKey = new Dictionary<string, string>() { { "Authorization", _authToken} },
+                    ApiKeyPrefix = new Dictionary<string, string>() { { "Authorization", "Bearer" } }
+                };
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Inizialize Authentication api (Authentication api not require authentication token)
+                var authApi = new IO.Swagger.Api.AuthenticationApi(_apiUrl);
+                //Login to obtain a valid token (and a refresh token)
+
+                /*
+                 *
+                 *
+                 *
+                 *      In the login procedure for the management API it is necessary to specify the additional scope ArxManagement to obtain a token enabled for management API
+                 *
+                 *
+                 *
+                 */
+
+
+                var resultToken = authApi.AuthenticationGetToken(new AuthenticationTokenRequestDTO(userTxt.Text, passwordTxt.Text, _appId, _secret,null, null, null, null, null, null, null, new List<string>{"ArxManagement"}));
+
+                _authToken = resultToken.AccessToken;
+                _refreshToken = resultToken.RefreshToken;
+
+                tokenLabel.Text = "Token presente";
+                tokenLabel.ForeColor = Color.Green;
+                tokenValue.Text = string.Empty;
+                //Print token information
+                foreach (var propertyInfo in resultToken.GetType().GetProperties())
+                {
+                    tokenValue.Text += string.Format("{0}: {1}{2}", propertyInfo.Name, propertyInfo.GetValue(resultToken), Environment.NewLine);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var aooManagementApi = new IO.Swagger.Management.Api.BusinessUnitsManagementApi(Configuration);
+            var aoos = aooManagementApi.BusinessUnitsManagementGet();
+            aooGrid.DataSource = aoos;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var documentTypesManagementApi = new IO.Swagger.Management.Api.DocumentTypesManagementApi(Configuration);
+            var documentTypes = documentTypesManagementApi.DocumentTypesManagementGetList();
+            docTypeGrid.DataSource = documentTypes;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (docTypeGrid.SelectedRows != null && docTypeGrid.SelectedRows.Count > 0)
+            {
+                var documentTypeId = ((DocumentTypeCompleteDTO) docTypeGrid.SelectedRows[0].DataBoundItem).Id;
+                    
+                var additionalFieldsManagementApi = new IO.Swagger.Management.Api.AdditionalFieldsManagementApi(Configuration);
+                var additionalFields = additionalFieldsManagementApi.AdditionalFieldsManagementGetAdditionalFieldsByDocumentTypeId(documentTypeId);
+                docTypeGrid.DataSource = additionalFields;
+            }
+        }
+    }
+}
